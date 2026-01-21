32c7df65bf1513e1df02a92c31f097f2702799e0
RafiaBonna <rafiaakterbonna@gmail.com>
2026-01-21 12:09:27 +0600

    my

--- New folder/SCM-Vue-Laravel/front-scm/src/components/Footer.vue
+++ New folder/SCM-Vue-Laravel/front-scm/src/components/Footer.vue
@@ -4,7 +4,7 @@
       Copyright &copy; {{ new Date().getFullYear() }}
     </strong>
     <br />
-    All rights reserved by Optichain 1
+    All rights reserved by Optichain 12
   </footer>
 </template>
 
